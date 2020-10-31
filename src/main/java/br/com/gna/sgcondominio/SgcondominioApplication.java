1e15d7239010cced798e7bef8c851d70a0bc7821
José Allison <jose_allison373@hotmail.com>
2020-10-31 00:56:42 -0300

    commit inicial

---
+++ src/main/java/br/com/gna/sgcondominio/SgcondominioApplication.java
@@ -0,0 +1,13 @@
+package br.com.gna.sgcondominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "br.com.gna.sgcondominio")
+public class SgcondominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcondominioApplication.class, args);
+	}
+
+}
