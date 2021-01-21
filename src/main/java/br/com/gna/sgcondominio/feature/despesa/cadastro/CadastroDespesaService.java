15b5408ebf06a464a5598a1973a252893029ba88
Allison <jose_allison373@gmail.com>
2021-01-21 00:40:25 -0300

    finalizacao do service do cadastro de despesa

--- src/main/java/br/com/gna/sgcondominio/feature/despesa/cadastro/CadastroDespesaService.java
+++ src/main/java/br/com/gna/sgcondominio/feature/despesa/cadastro/CadastroDespesaService.java
@@ -1,20 +1,25 @@
 package br.com.gna.sgcondominio.feature.despesa.cadastro;
 
+import br.com.gna.sgcondominio.entities.DespesaEntity;
 import br.com.gna.sgcondominio.feature.despesa.cadastro.model.CadastroDespesaForm;
 import br.com.gna.sgcondominio.feature.despesa.cadastro.model.CadastroDespesaView;
+import br.com.gna.sgcondominio.repository.DespesaRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Service;
 
 @Service
-@Log4j
 @RequiredArgsConstructor
 public class CadastroDespesaService {
 
+    private final DespesaRepository despesaRepository;
+
     private final CadastroDespesaConverter cadastroDespesaConverter;
 
-    public CadastroDespesaView postCadastroDespesa (CadastroDespesaForm form) {
-
+    public CadastroDespesaView postCadastroDespesa(CadastroDespesaForm form) {
+        DespesaEntity despesaEntity = cadastroDespesaConverter.toDespesaEntity(form);
+        despesaEntity = despesaRepository.save(despesaEntity);
+        return cadastroDespesaConverter.toCadastroDespesaView(despesaEntity);
     }
 
 }
